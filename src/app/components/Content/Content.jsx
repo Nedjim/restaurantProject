47bd6f48aab1a0dd61f630504fe5521a6a212fa8
Nedjim DANIMON NGABA <nedjim.danimon@gmail.com>
2017-02-08 22:57:12 +0100

    MAJ: content css

--- src/app/components/Content/Content.jsx
+++ src/app/components/Content/Content.jsx
@@ -17,29 +17,27 @@ export default class Content extends React.Component {
     render() {
         this.search();
         return (
-            <div id="content">
-                <div id="container">
-                    <h1>Carte restaurant</h1>
-                    <div className="description">
-                        <h2>Description du plat</h2>
+                <div id="content">
+                   <h1 className="title">Carte du restaurant</h1>
+                   <div className="plat">
+                        <h3>Nom du plat</h3>
                         <img src="http://www.clairemarais.fr/client/gfx/photos/produit/restaurant3_7.jpg"/>
 
-                        <p className="text">Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.<br/> Le Lorem Ipsum est le faux texte standard de l/'imprimerie depuis les années 1500<br />Le Lorem Ipsum est simplement du<br /> faux texte employé dans la composition et la mise en page avant impression.<br /> Le Lorem Ipsum est le faux texte standard de l/'imprimerie depuis les années 1500</p>
+                         <p className="text">Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.<br/> Le Lorem Ipsum est le faux texte standard de l/'imprimerie depuis les années 1500<br />Le Lorem Ipsum est simplement du<br /> faux texte employé dans la composition et la mise en page avant impression.<br /> Le Lorem Ipsum est le faux texte standard de l/'imprimerie depuis les années 1500</p>
                     </div>
 
-                    <div className="description">
-                        <h2>Description du plat</h2>
+                    <div className="plat">
+                        <h3>Nom du plat</h3>
                         <img src="http://www.clairemarais.fr/client/gfx/photos/produit/restaurant3_7.jpg"/>
                         <p className="text2">Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.<br /> Le Lorem Ipsum est le faux texte standard de l/'imprimerie depuis les années 1500<br />Le Lorem Ipsum est simplement du<br /> faux texte employé dans la composition et la mise en page avant impression.<br /> Le Lorem Ipsum est le faux texte standard de l/'imprimerie depuis les années 1500</p>
                     </div>
 
-                    <div className="description">
-                        <h2>Description du plat</h2>
+                    <div className="plat">
+                        <h3>Nom du plat</h3>
                         <img src="http://www.clairemarais.fr/client/gfx/photos/produit/restaurant3_7.jpg"/>
                         <p className="text3">Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.<br /> Le Lorem Ipsum est le faux texte standard de l/'imprimerie depuis les années 1500<br />Le Lorem Ipsum est simplement du<br /> faux texte employé dans la composition et la mise en page avant impression.<br /> Le Lorem Ipsum est le faux texte standard de l/'imprimerie depuis les années 1500</p>
                     </div>
                 </div>
-            </div>
         );
     }
 }
