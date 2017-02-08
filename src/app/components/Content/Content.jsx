cbdb60dc8834cdae187fbef289c12b2407279aaf
Stéphane <Stephane.lelay93@gmail.com>
2017-02-08 15:34:49 +0100

    add content

--- src/app/components/Content/Content.jsx
+++ src/app/components/Content/Content.jsx
@@ -6,26 +6,26 @@ export default class Content extends React.Component {
             <div>
                      <h1>Carte restaurant</h1>
                     <div id="container">
-                        <div class="Description">
+                        <div className="Description">
                             <h2>Description du plat</h2>
                             <img src="http://www.clairemarais.fr/client/gfx/photos/produit/restaurant3_7.jpg"/>
 
-                            <p class="text">Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.<br/> Le Lorem Ipsum est le faux texte standard de l/'imprimerie depuis les années 1500<br />Le Lorem Ipsum est simplement du<br /> faux texte employé dans la composition et la mise en page avant impression.<br /> Le Lorem Ipsum est le faux texte standard de l/'imprimerie depuis les années 1500</p>
+                            <p className="text">Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.<br/> Le Lorem Ipsum est le faux texte standard de l/'imprimerie depuis les années 1500<br />Le Lorem Ipsum est simplement du<br /> faux texte employé dans la composition et la mise en page avant impression.<br /> Le Lorem Ipsum est le faux texte standard de l/'imprimerie depuis les années 1500</p>
                         </div>
 
 
-                        <div class="Description2">
+                        <div className="Description2">
                             <h2>Description du plat</h2>
 
                             <img src="http://www.clairemarais.fr/client/gfx/photos/produit/restaurant3_7.jpg"/>
-                            <p class="text2">Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.<br /> Le Lorem Ipsum est le faux texte standard de l/'imprimerie depuis les années 1500<br />Le Lorem Ipsum est simplement du<br /> faux texte employé dans la composition et la mise en page avant impression.<br /> Le Lorem Ipsum est le faux texte standard de l/'imprimerie depuis les années 1500</p>
+                            <p className="text2">Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.<br /> Le Lorem Ipsum est le faux texte standard de l/'imprimerie depuis les années 1500<br />Le Lorem Ipsum est simplement du<br /> faux texte employé dans la composition et la mise en page avant impression.<br /> Le Lorem Ipsum est le faux texte standard de l/'imprimerie depuis les années 1500</p>
                         </div>
 
-                        <div class="Description3">
+                        <div className="Description3">
                             <h2>Description du plat</h2>
 
                             <img src="http://www.clairemarais.fr/client/gfx/photos/produit/restaurant3_7.jpg"/>
-                            <p class="text3">Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.<br /> Le Lorem Ipsum est le faux texte standard de l/'imprimerie depuis les années 1500<br />Le Lorem Ipsum est simplement du<br /> faux texte employé dans la composition et la mise en page avant impression.<br /> Le Lorem Ipsum est le faux texte standard de l/'imprimerie depuis les années 1500</p>
+                            <p className="text3">Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.<br /> Le Lorem Ipsum est le faux texte standard de l/'imprimerie depuis les années 1500<br />Le Lorem Ipsum est simplement du<br /> faux texte employé dans la composition et la mise en page avant impression.<br /> Le Lorem Ipsum est le faux texte standard de l/'imprimerie depuis les années 1500</p>
                         </div>
                   </div>
 
