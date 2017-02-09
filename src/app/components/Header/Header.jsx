b80ecf49e01a52a6954cffadab8eeb3348b54109
moano <moanodadou2@gmail.com>
2017-02-09 12:44:59 +0100

    add header

--- src/app/components/Header/Header.jsx
+++ src/app/components/Header/Header.jsx
@@ -3,9 +3,22 @@ import React from 'react';
 export default class Header extends React.Component {
     render() {
         return (
+
             <header>
-               Header
+              <div id="menu">
+                  <ul id="nav">
+                      <li><a href="Accueil">Acceuil</a></li>
+                      <li><a href="Carte">Carte</a></li>
+                      <li><a href="Contact">Contact</a></li>
+                      <li><a href="Panier">Panier</a></li>
+                  </ul>
+                  <div id="logo">
+                      <img src="../../img/header/la.png" alt=""/>
+                  </div>
+              </div>
             </header>
+
+
         );
     }
 }
