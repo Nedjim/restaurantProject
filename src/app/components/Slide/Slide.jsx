220a7b6a83fc50df7a4acb4724e9dd961d9634b0
nessima <nessima.yakil@gmailcom>
2017-02-09 12:55:29 +0100

    add img slide

--- src/app/components/Slide/Slide.jsx
+++ src/app/components/Slide/Slide.jsx
@@ -5,9 +5,9 @@ export default class Slide extends React.Component {
         return (
             <div id="slide">
                 <ul id='carrousel'>
-                    <li><img src="https://media-cdn.tripadvisor.com/media/photo-s/02/1b/9a/bf/le-grande-bouffe.jpg"/></li>
-                    <li><img src="http://www.epicurien.be/img/recettes-cuisines/20130928_poulet_morilles-500.jpg"/></li>
-                    <li><img src="https://i1.wp.com/cotesoleils.fr/wp-content/uploads/2015/06/tajine-agneau-et-pruneaux-algc3a9rie1.jpg"/></li>
+                    <li><img src="restau.jpeg"/></li>
+                    <li><img src="interieur.jpeg"/></li>
+                    <li><img src="poulet.jpeg"/></li>
                 </ul>
             </div>
         );
