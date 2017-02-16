e1926e995cfb0a8f4296b316ef344beb1752a257
helene <helene.niassy@gmail.com>
2017-02-16 15:50:56 +0100

    add map

--- src/app/components/Map/Map.jsx
+++ src/app/components/Map/Map.jsx
@@ -5,7 +5,7 @@ export default class Map extends React.Component {
         return (
             <div id="map">
                  <h2>Où sommes nous ? </h2>
-               <img src='app/img/map/map.jpg'/>
+               <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2625.209196894807!2d2.434700750711676!3d48.85422110885359!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66d5f8a4cb333%3A0x9535ab9bdafaa332!2s55+Rue+de+Vincennes%2C+93100+Montreuil!5e0!3m2!1sfr!2sfr!4v1487255448184" width="80%" height="450"  ></iframe>
             </div>
         );
     }
